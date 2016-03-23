3428bd21dcdbcc272f9859c383755509d46d5c0d
kate hall and Nickolas Muenchow <nickolas@>
2016-03-23 10:35:50 -0700

    it will submit madlib form into text paragraph

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
-  
+  $("#blanks form").submit(function() {
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput = $("input#animal").val();
+    var verbInput = $("input#verb").val();
 
 
 
+    $(".person1").append(person1Input);
+    $(".person2").append(person2Input);
+    $(".animal").append(animalInput);
+    $(".verb").append(verbInput);
 
+    $("#story").show();
 
-
+    event.preventDefault();
+});
 });
